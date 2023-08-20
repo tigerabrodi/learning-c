4654ab429f815f0b53624752ac8eabefcfd24519
Tiger Abrodi <tigerabrodi@gmail.com>
2023-08-20 11:52:14 +0200

    commit

--- printf/functions.c
+++ printf/functions.c
@@ -23,57 +23,37 @@ void my_printf(const char *format, ...)
 	va_start(args, format);
 
 	int index = 0;
-	int argument_to_be_added_index = 0;
-
-	size_t format_length = strlen(format);
-	char *full_string_to_be_printed = (char *)malloc(*format + 1);
-	char *arguments = (char *)malloc(*format + 1);
-
-	// How do I distinguish between the print string and the arguments passed?
-	// Is this the correct way to iterate through the arguments alone?
-	while (va_arg(args, int))
-	{
-		int argument_list_index = 0;
-		arguments[argument_index] = va_arg(args, int);
-		argument_index++;
-	}
+	char *full_string_to_be_printed = (char *)malloc(strlen(format) * 2); // Assuming enough space
 
 	if (full_string_to_be_printed == NULL)
 	{
-		my_printf("Memory allocation failed\n");
+		my_puts("Memory allocation failed\n");
 		va_end(args);
+		return;
 	}
 
-	while (*format != "\0")
+	while (format[index] != '\0') // Keep looping until the end of the string
 	{
-		if (format[index] == "%")
+		if (format[index] == '%') // Check if the current character is the start of a format specifier
 		{
-			const char type_to_be_printed_index = index++;
-			if (format[type_to_be_printed_index] == "d")
+			index++;		  // Move past the '%' to the character that specifies the type
+			if (format[index] == 'd') // If it's 'd', then an integer argument is expected
 			{
-				// Keep track of the amount of integers already accessed?
-				// How do I know the position of each
-
-				// check if type is integer (how do I do this and turn it into an integer from a string?)
-				if (arguments[argument_to_be_added_index])
-				{
-					// add integer to string at index
-					full_string_to_be_printed[index] = arguments[argument_to_be_added_index];
-				}
-				else
-				{
-					my_printf("Error: Argument type does not match format specifier\n");
-					va_end(args);
-				}
+				int integer_arg = va_arg(args, int); // Get the next argument as an integer
+								     // TODO: Convert integer_arg to string
+								     // TODO: Concatenate the string to full_string_to_be_printed
 			}
+			// TODO: Handle other format specifiers if needed
 		}
 		else
 		{
-			full_string_to_be_printed[index] = format[index];
+			// TODO: Add the character to full_string_to_be_printed since it's not part of a format specifier
 		}
 
-		index++;
+		index++; // Move to the next character in the format string
 	}
 
 	my_puts(full_string_to_be_printed);
+	free(full_string_to_be_printed); // Free allocated memory
+	va_end(args);
 }
