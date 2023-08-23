2c60acb77e70bd05efe42a2eec9390ef5319d5e6
Tiger Abrodi <tigerabrodi@gmail.com>
2023-08-23 07:52:20 +0200

    implement strcmp

--- c-functions/main.c
+++ c-functions/main.c
@@ -12,6 +12,9 @@ int main()
 	size_t length1 = my_strlen(testString1);
 	size_t length2 = my_strlen(testString2);
 
+	printf("This should be zero %d ", my_strcmp(testString1, testString2));
+	printf("This should be one %d ", my_strcmp("hey", "hey"));
+
 	printf("Length of \"%s\": %zu\n", testString1, length1);
 	printf("Length of \"%s\": %zu\n", testString2, length2);
 
@@ -30,4 +33,23 @@ size_t my_strlen(const char *str)
 	}
 
 	return length;
+}
+
+int my_strcmp(const char *str1, const char *str2)
+{
+	int is_same = 1;
+
+	while (*str1 != '\0' && is_same == 1 && *str2 != '\0')
+	{
+
+		if (*str1 != *str2)
+		{
+			return 0;
+		}
+
+		str1++;
+		str2++;
+	}
+
+	return (*str1 == '\0' && *str2 == '\0') ? 1 : 0;
 }
