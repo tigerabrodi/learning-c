6577ad8beec65993e3148234ade22b9f16bb7a08
Tiger Abrodi <tigerabrodi@gmail.com>
2023-08-23 07:38:59 +0200

    strlen

--- c-functions/main.c
+++ c-functions/main.c
@@ -1,5 +1,33 @@
+#include <stdio.h>
+
+// Function Prototype
+size_t my_strlen(const char *str);
+int my_strcmp(const char *str1, const char *str2);
 
 int main()
 {
+	const char *testString1 = "Hello, World!";
+	const char *testString2 = "OpenAI";
+
+	size_t length1 = my_strlen(testString1);
+	size_t length2 = my_strlen(testString2);
+
+	printf("Length of \"%s\": %zu\n", testString1, length1);
+	printf("Length of \"%s\": %zu\n", testString2, length2);
+
 	return 0;
+}
+
+// Function Definition
+size_t my_strlen(const char *str)
+{
+	size_t length = 0;
+
+	while (*str != '\0')
+	{
+		length++;
+		str++;
+	}
+
+	return length;
 }
