2502c8c46039d061965930eb5fcc472423378d52
Tiger Abrodi <tigerabrodi@gmail.com>
2023-08-24 09:25:15 +0200

    run all

--- testing-library/tests.c
+++ testing-library/tests.c
@@ -1,24 +1,36 @@
 #include "tests.h"
 #include <stdio.h>
 
+int total_tests = 0;
+int passed_tests = 0;
+int failed_tests = 0;
+
 void assert_true(int condition, const char *test_name)
 {
+	total_tests++;
 	if (!condition)
 	{
 		printf("[ERROR] Test '%s' failed.\n", test_name);
+		failed_tests++;
+		return;
 	}
 
 	printf("[PASSING] Test '%s' passed.\n", test_name);
+	passed_tests++;
 }
 
 void assert_false(int condition, const char *test_name)
 {
+	total_tests++;
 	if (condition)
 	{
 		printf("[ERROR] Test '%s' failed.\n", test_name);
+		failed_tests++;
+		return;
 	}
 
 	printf("[PASSING] Test '%s' passed.\n", test_name);
+	passed_tests++;
 }
 
 int test_count = 0;
@@ -42,4 +54,9 @@ void run_all_tests()
 	{
 		registered_tests[i]();
 	}
+
+	printf("\n=== TEST SUMMARY ===\n");
+	printf("Total Tests: %d\n", total_tests);
+	printf("Passed: %d\n", passed_tests);
+	printf("Failed: %d\n", failed_tests);
 }
